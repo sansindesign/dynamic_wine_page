ea5fde3e65dcd2eabc9fad95895c5672a09ffbfd
natashalefort <pavelmoulisjr@gmail.com>
2019-02-28 20:01:36 +0100

    cleanup

--- new.js
+++ new.js
@@ -5,25 +5,21 @@ const productListLink = "https://spreadsheets.google.com/feeds/list/1s4CkxsGQ93R
 
 function loadJSON(link) {
     console.log(link);
-    fetch(link).then(e=>e.json()).then(data=>data.feed.entry.forEach(displayWineData))
+    fetch(link).then(e => e.json()).then(data => data.feed.entry.forEach(displayWineData))
 }
-let count=0;
+let count = 0;
+
 function displayWineData(product) {
-//    console.log(product)
-    //const section = document.querySelector("." + product.category)
-
-    const section = document.querySelector("."+product.gsx$country.$t);
-
+    //    console.log(product)
+    const section = document.querySelector("." + product.gsx$country.$t);
     let clone = template.cloneNode(true);
-    clone.querySelector(".wineWrapper", "#myBtn").id+=count;
+    clone.querySelector(".wineWrapper", "#myBtn").id += count;
     clone.querySelector(".wineImage").src = product.gsx$wineimage.$t;
-    //clone.querySelector("img").alt = product.image;
     clone.querySelector(".name").textContent = product.gsx$name.$t;
     clone.querySelector(".wineHouse").textContent = product.gsx$house.$t;
     clone.querySelector(".area").textContent = product.gsx$region.$t;
     clone.querySelector(".rating").textContent = product.gsx$rating.$t;
     clone.querySelector(".ratingImage").src = product.gsx$ratingimage.$t;
-
     clone.querySelector(".modal", "#myModal");
     clone.querySelector(".modalcontent");
     clone.querySelector(".close");
@@ -34,23 +30,21 @@ function displayWineData(product) {
     clone.querySelector(".modalrating").textContent = product.gsx$rating.$t;
     clone.querySelector(".modalratingimg").src = product.gsx$ratingimage.$t;
     clone.querySelector(".modaldescription").textContent = product.gsx$description.$t;
-    clone.querySelector(".acidity").textContent = "acidity: "+product.gsx$acidity.$t;
-    clone.querySelector(".abv").textContent = "alcohol: "+product.gsx$alcoholprecentage.$t;
-    clone.querySelector(".cost").textContent = "average price: "+product.gsx$price.$t+"dkk";
+    clone.querySelector(".acidity").textContent = "acidity: " + product.gsx$acidity.$t;
+    clone.querySelector(".abv").textContent = "alcohol: " + product.gsx$alcoholprecentage.$t;
+    clone.querySelector(".cost").textContent = "average price: " + product.gsx$price.$t + "dkk";
+    clone.querySelector(".wineWrapper", "#myBtn" + count).onclick = function (e) {
 
+        this.nextElementSibling.style.display = "block";
+    }
 
-   clone.querySelector(".wineWrapper", "#myBtn"+count).onclick=  function(e) {
+    clone.querySelector(".close").onclick = function () {
+        this.parentNode.parentNode.style.display = "none";
+    }
+    //document.querySelector("#winegrid").appendChild(clone)
+    section.appendChild(clone)
 
-     this.nextElementSibling.style.display = "block";
-        }
-
-clone.querySelector(".close").onclick = function() {
-  this.parentNode.parentNode.style.display = "none";
-}
-  //document.querySelector("#winegrid").appendChild(clone)
-  section.appendChild(clone)
-
-   count++;
+    count++;
 };
 
 loadJSON(productListLink)
